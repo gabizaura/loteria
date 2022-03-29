cd50713ed1df08bfafdc1393c63282ba84aafff6
JenniBehnen <100212625+JenniBehnen@users.noreply.github.com>
2022-03-29 10:57:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
 var numSort= []
-var cont = 0
+var numEsco = []
 
 function sorteio(){
+    var cont = 0
+    numSort =[]
     while(cont < 6){
         let num = Math.random() * 60
         num = Math.ceil(num)
-        console.log(num)
-        cont ++
+        if(!numSort.includes(num)){
+            numSort[cont] = num
+            console.log(numSort)
+            cont ++
+        }
+        
     }
+
+    document.getElementById("sorteados").innerHTML = numSort
+    contAcertos()
 }
 
-sorteio()
+function getValor(valor, pos){
+    valor = Number(valor)
+    if(valor <= 0 || valor > 60){
+        alert("Numero invalido, digite um  entre 1 e 60")
+        document.getElementById(`num${pos+1}`).value= ""
+    }else if (numEsco.includes(valor)){
+        alert("Numero repetido, escolha um outro valor")
+        document.getElementById(`num${pos+1}`).value= ""
+    }else{
+        numEsco[pos] = valor
+        console.log(numEsco)
+    }
+    
+}
+
+function contAcertos(){
+    var contA = 0
+    numEsco.forEach(function(value, index){
+        if(numSort.includes(value)){
+            contA++
+        }
+    })
+    document.getElementById("acertos").innerHTML = contA
+}

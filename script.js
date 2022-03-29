076002504b0d1af22179160f0a4e223494714d85
gabizaura <gabriellizaura@gmail.com>
2022-03-29 14:00:17 -0400

    arrumando o código de sorteio

--- script.js
+++ script.js
@@ -1,44 +1,50 @@
-var numSort= []
+var numSort = []
 var numEsco = []
 
 function sorteio(){
+    if(numEsco.length == 6){
     var cont = 0
-    numSort =[]
+    numSort = []
+
     while(cont < 6){
-        let num = Math.random() * 60
-        num = Math.ceil(num)
-        if(!numSort.includes(num)){
-            numSort[cont] = num
-            console.log(numSort)
-            cont ++
-        }
-        
+    let num = Math.random() * 60
+    num = Math.ceil(num)
+    if(!numSort.includes(num)){
+    numSort[cont] = num
+    console.log(numSort)
+    cont++
     }
 
+    }
+
+
     document.getElementById("sorteados").innerHTML = numSort
+ 
     contAcertos()
+}else{
+    alert("É necessário informar 6 números para sortear")
+}
 }
 
 function getValor(valor, pos){
     valor = Number(valor)
-    if(valor <= 0 || valor > 60){
-        alert("Numero invalido, digite um  entre 1 e 60")
-        document.getElementById(`num${pos+1}`).value= ""
-    }else if (numEsco.includes(valor)){
-        alert("Numero repetido, escolha um outro valor")
-        document.getElementById(`num${pos+1}`).value= ""
-    }else{
-        numEsco[pos] = valor
-        console.log(numEsco)
+    if(valor < 0 || valor > 60){
+        alert("Número inválido, digite um número entre 1 e 60")
+        document.getElementById(`num${pos+1}`).value = " "
+    } else if (numEsco.includes(valor)){
+        alert("Número repetido, escolha um outro número")
+        document.getElementById(`num${pos+1}`).value = " "
+    } else{
+    numEsco[pos] = valor
+    console.log(numEsco)
     }
-    
 }
 
 function contAcertos(){
     var contA = 0
     numEsco.forEach(function(value, index){
         if(numSort.includes(value)){
-            contA++
+            contA = contA + 1
         }
     })
     document.getElementById("acertos").innerHTML = contA
